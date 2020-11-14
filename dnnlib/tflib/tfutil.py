237dc0a75f390d6616e9082ca3a8e4784cc8efb2
Bayangmbe Mounmo <bayangp0@gmail.com>
2020-11-14 15:55:45 +0100

    Add check part for tensorflow 2 | reset Session to comapt.v1 from TensorFlow

--- dnnlib/tflib/tfutil.py
+++ dnnlib/tflib/tfutil.py
@@ -8,7 +8,8 @@
 """Miscellaneous helper utils for Tensorflow."""
 
 import numpy as np
-import tensorflow as tf
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 from typing import Any, Iterable, List, Union
 
@@ -30,7 +31,7 @@ def is_tf_expression(x: Any) -> bool:
     return isinstance(x, (tf.Tensor, tf.Variable, tf.Operation))
 
 
-def shape_to_list(shape: Iterable[tf.Dimension]) -> List[Union[int, None]]:
+def shape_to_list(shape: Iterable[tf.compat.v1.Dimension]) -> List[Union[int, None]]:
     """Convert a Tensorflow shape to a list of ints."""
     return [dim.value for dim in shape]
 
